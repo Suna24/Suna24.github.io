8c600b8977a7bc4aaf28590652b51de88e65350b
Suna24 <bastien.t2002@gmail.com>
2022-01-26 16:30:40 -0500

    replace all infos + beginning of the slider

---
+++ assets/js/swiper.js
@@ -0,0 +1,15 @@
+
+swiper = new Swiper('.swiper', {
+  
+    //Pagination
+    pagination: {
+        el: '.swiper-pagination',
+    },
+
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+});
+
