674ad7dea44853fc5f0e8d1ab3991a0577bd5f51
Suna24 <bastien.t2002@gmail.com>
2022-01-29 22:56:14 -0500

    move on the good project

--- assets/js/swiper.js
+++ assets/js/swiper.js
@@ -1,6 +1,12 @@
-
+//Instance du swiper
 swiper = new Swiper('.swiper', {
   
+  //Navigation using Keyboard
+    keyboard: {
+      enabled: true,
+      onlyInViewport: false,
+    },
+
     //Pagination
     pagination: {
         el: '.swiper-pagination',
@@ -12,4 +18,21 @@ swiper = new Swiper('.swiper', {
       prevEl: '.swiper-button-prev',
     },
 });
+
+//Fonction pour avoir l'index du projet
+function getIndex(){
+  let index = window.location.hash;
+  index = index.substring(1);
+  console.log(index);
+
+  return parseInt(index);
+}
+
+swiper.slideTo(getIndex(), 0);
+
+//Listener
+swiper.on('slideChange', function () {
+  window.location.hash = swiper.activeIndex;
+});
+
 
