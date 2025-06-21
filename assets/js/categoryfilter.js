88aa85e20d00e5928d16e2cd76f784e3164017a9
btarot <btarot@et.esiea.fr>
2025-06-21 15:32:23 +0200

    add button to filter projects

---
+++ assets/js/categoryfilter.js
@@ -0,0 +1,22 @@
+const buttons = document.querySelectorAll(".filter-button");
+const sections = document.querySelectorAll("#two > section");
+
+buttons.forEach((button) => {
+  button.addEventListener("click", () => {
+    const filter = button.getAttribute("data-filter");
+
+    // Remove 'active' class from all buttons
+    buttons.forEach((btn) => btn.classList.remove("active"));
+
+    // Add 'active' class to the clicked button
+    button.classList.add("active");
+
+    sections.forEach((section) => {
+      if (filter === "all" || section.classList.contains(filter)) {
+        section.style.display = "flex";
+      } else {
+        section.style.display = "none";
+      }
+    });
+  });
+});
