7c400a171a62ad4d46b54bbaa0934ba31afbd33c
btarot <btarot@et.esiea.fr>
2025-06-21 20:21:41 +0200

    finishing translating main page

--- assets/js/i18n.js
+++ assets/js/i18n.js
@@ -36,11 +36,46 @@ $(function () {
               intro: {
                 title: "Welcome to my portfolio",
                 cv: "View my CV",
+                introducing:
+                  "I am Bastien TAROT, a final year engineering student at <b>ESIEA</b>, specializing in <b>Artificial Intelligence & Data Science</b>. Here you will find many projects I have completed during my studies and in my free time!",
+                passion:
+                  "Although most of the projects you will find here are related to the use of AI, I am also passionate about <b>Cybersecurity</b>, a field in which I am training and completing challenges on the TryHackMe platform.",
+                teasing:
+                  "Feel free to contact me if you have any questions or if you want to know more about my profile!",
               },
               education: {
                 title: "Education",
                 description:
                   "You will find below the different stages of my educational path.",
+                high_school: {
+                  title: "Auguste et Jean Renoir High School in ANGERS",
+                  diploma:
+                    "Scientific Baccalaureat with a specialization in Computer Science",
+                },
+                dut: {
+                  title: "LAVAL IUT",
+                  diploma:
+                    "University Diploma of Technology in Computer Science",
+                },
+                esiea: {
+                  title: "ESIEA Laval",
+                  diploma:
+                    "[In progress] - Engineering Degree<br>Specialization in AI & Data Science",
+                },
+                transition_to_abroad:
+                  "During my studies, I also had the opportunity to complete two semesters abroad.",
+                canada: {
+                  date: "January - June 2022",
+                  title: "CEGEP of Matane, Canada",
+                  description:
+                    "Study semester in Quebec: Computer Science Techniques",
+                },
+                croatia: {
+                  date: "February - June 2023",
+                  title: "University of Zagreb, Croatia",
+                  description:
+                    "Study semester in Croatia at the Faculty of Computer Science in Varazdin",
+                },
               },
               experience: {
                 title: "Professional experiences",
@@ -65,6 +100,24 @@ $(function () {
               },
               skills: {
                 title: "My skills",
+                description:
+                  'During my years of studies and my experiences in companies, I have acquired many skills, both technical and human. I have been able to apply these skills in numerous projects that you will find just <a href="#projects">here</a>.',
+                ai: '<span class="icon solid major fa-solid fa-hexagon-nodes-bolt"></span><h3>Artificial Intelligence</h3><ul><li>Machine Learning</li><li>Natural Language Processing</li><li>Generative AI</li><li>RAG Architectures</li><li>Computer Vision</li></ul>',
+                data: '<span class="icon solid major fa-solid fa-chart-line"></span><h3>Analyzing, preparing, and visualizing data</h3><ul><li>Libraries scikit-learn, numpy, pandas, matplotlib</li><li>Visualization software TABLEAU</li><li>Big Data: Apache Spark</li></ul>',
+                programming:
+                  '<span class="icon solid major fa-file-code"></span><h3>Developing programs</h3><ul><li>Programming languages: Python, Java, C++, C, SQL</li><li>Object-oriented programming</li></ul>',
+                database:
+                  '<span class="icon solid major fa-database"></span><h3>Administering and querying databases</h3><ul><li>PL/SQL (Stored Procedures, Views, Rights)</li><li>PostgreSQL, MongoDB, MySQL</li></ul>',
+                git: '<span class="icon solid major brands fa-github"></span><h3>Collaborating with Git</h3><ul><li>Git bash</li><li>GitHub & GitLab</li><li>GitHub Desktop</li></ul>',
+                virtualization:
+                  '<span class="icon solid major brands fa-docker"></span><h3>Virtualization and Deployment</h3><ul><li>Tools: Docker, Ansible, VirtualBox</li><li>CI/CD: GitLab CI, GitHub Actions</li><li>Google Cloud Platform</li><li>MLOps</li></ul>',
+                testing:
+                  '<span class="icon solid major fa-solid fa-robot"></span><h3>Conducting and automating tests</h3><ul><li>Automation: Selenium, Protractor, Cucumber</li><li>Bug reporting</li><li>Non-regression testing</li></ul>',
+                ide: '<span class="icon solid major fa-solid fa-code"></span><h3>Development software</h3><ul><li>Visual Studio Code</li><li>Visual Studio</li><li>Jupyter Notebook</li><li>JetBrains Suite (Intellji, Pycharm, PhpStorm)</li></ul>',
+                teamwork:
+                  '<span class="icon solid major fa-users"></span><h3>Teamwork & Communication</h3><ul><li>Listening</li><li>Adaptability</li><li>Team spirit</li></ul>',
+                management:
+                  '<span class="icon solid major fas fa-project-diagram"></span><h3>Project Management</h3><ul><li>SCRUM methods and agile teamwork</li><li>Software: Jira, Trello, Confluence</li></ul>',
               },
               projects: {
                 title: "My projects",
@@ -151,11 +204,46 @@ $(function () {
               intro: {
                 title: "Bienvenue sur mon portfolio",
                 cv: "Voir mon CV",
+                introducing:
+                  "Je suis Bastien TAROT, étudiant en dernière année d'école d'ingénieur à <b>l'ESIEA</b>, avec une spécialisation en <b>Intelligence Artificielle & Data Science</b>. Vous trouverez ici, de nombreux projets que j'ai pu réaliser durant mes années d'études et sur mon temps libre !",
+                passion:
+                  "Bien que la majorité des projets que vous trouverez ici soient liés à l'utilisation de l'IA, je suis également passionné par la <b>Cybersécurité</b>, domaine dans lequel je me forme et réalise des challenges sur la plateforme TryHackMe.",
+                teasing:
+                  "N'hésitez pas à me contacter si vous avez des questions ou si vous souhaitez en savoir plus sur mon profil !",
               },
               education: {
                 title: "Formation",
                 description:
                   "Vous trouverez ci-dessous les différentes étapes de mon parcours scolaire.",
+                high_school: {
+                  title: "Lycée Auguste et Jean Renoir à ANGERS",
+                  diploma:
+                    "Baccalauréat Scientifique spécialité Informatique et Sciences du Numérique",
+                },
+                dut: {
+                  title: "IUT de LAVAL",
+                  diploma:
+                    "Diplôme Universitaire de Technologie en Informatique",
+                },
+                esiea: {
+                  title: "ESIEA Laval",
+                  diploma:
+                    "[En cours] - Diplôme d'ingénieur<br>Majeure IA & Data Science",
+                },
+                transition_to_abroad:
+                  "Durant mon cursus, j'ai également eu l'opportunité de réaliser deux semestres d'études à l'étranger.",
+                canada: {
+                  date: "Janvier - Juin 2022",
+                  title: "CEGEP de Matane, Canada",
+                  description:
+                    "Semestre d'étude au Québec : Techniques de l'informatique",
+                },
+                croatia: {
+                  date: "Février - Juin 2023",
+                  title: "Université de Zagreb, Croatie",
+                  description:
+                    "Semestre d'étude en Croatie à la Faculté d'informatique de Varazdin",
+                },
               },
               experience: {
                 title: "Expériences professionnelles",
@@ -180,6 +268,24 @@ $(function () {
               },
               skills: {
                 title: "Mes compétences",
+                description:
+                  "Durant mes années de formation et mes expériences en entreprises, j'ai pu acquérir de nombreuses compétences aussi bien informatiques et techniques qu'humaines. J'ai pu mettre en application ces compétences lors de nombreux projets que vous trouverez juste <a href=\"#projects\">ici</a>.",
+                ai: '<span class="icon solid major fa-solid fa-hexagon-nodes-bolt"></span><h3>Intelligence Artificielle</h3><ul><li>Apprentissage Machine</li><li>Traitement du langage natural</li><li>IA générative</li><li>Architectures RAG</li><li>Vision par ordinateur</li></ul>',
+                data: '<span class="icon solid major fa-solid fa-chart-line"></span><h3>Analyser, préparer et visualiser des données</h3><ul><li>Librairies scikit-learn, numpy, pandas, matplotlib</li><li>Logiciel de visualisation TABLEAU</li><li>Big Data : Apache Spark</li></ul>',
+                programming:
+                  '<span class="icon solid major fa-file-code"></span><h3>Développer des programmes</h3><ul><li>Langages de programmation : Python, Java, C++, C, SQL</li><li>Programmation orientée objet</li></ul>',
+                database:
+                  '<span class="icon solid major fa-database"></span><h3>Administrer et requêter des bases de données</h3><ul><li>PL/SQL (Procédures stockées, Vues, Droits)</li><li>PostgreSQL, MongoDB, MySQL</li></ul>',
+                git: '<span class="icon solid major brands fa-github"></span><h3>Collaborer avec Git</h3><ul><li>Git bash</li><li>GitHub & GitLab</li><li>GitHub Desktop</li></ul>',
+                virtualization:
+                  '<span class="icon solid major brands fa-docker"></span><h3>Virtualisation et Déploiement</h3><ul><li>Outils : Docker, Ansible, VirtualBox</li><li>CI/CD : GitLab CI, GitHub Actions</li><li>Google Cloud Platform</li><li>MLOps</li></ul>',
+                testing:
+                  '<span class="icon solid major fa-solid fa-robot"></span><h3>Conduite et automatisation de tests</h3><ul><li>Automatisation : Selenium, Protractor, Cucumber</li><li>Remontée de bugs</li><li>Tests de non-regression</li></ul>',
+                ide: '<span class="icon solid major fa-solid fa-code"></span><h3>Logiciel de développement</h3><ul><li>Visual Studio Code</li><li>Visual Studio</li><li>Notebook Jupyter</li><li>Suite JetBrains (Intellji, Pycharm, PhpStorm)</li></ul>',
+                teamwork:
+                  "<span class=\"icon solid major fa-users\"></span><h3>Travail d'équipe & Communication</h3><ul><li>A l'écoute</li><li>Adaptabilité</li><li>Esprit d'équipe</li></ul>",
+                management:
+                  '<span class="icon solid major fas fa-project-diagram"></span><h3>Gestion de projet</h3><ul><li>Méthodes SCRUM et fonctionnement agile</li><li>Logiciels Jira, Trello, Confluence</li></ul>',
               },
               projects: {
                 title: "Mes projets",
